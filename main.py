8cf60eea1a38365b8d5c99eb802c3f76acab5dbd
ArielMoise <ariel.moise@anecdotes.ai>
2022-04-19 13:07:46 +0300

    workflow_dispact

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ input_variable = os.environ['INPUT_STORE']
 def main():
     with open('readme.txt', 'w') as f:
         f.write(input_variable)
+    print(input_variable)
 
 main()
-print("input_variable")
-print(input_variable)
